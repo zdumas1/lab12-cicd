88b7519bfcf586d1a6692723a3bb4086d8c8ab45
zdumas1 <zdumas1@emich.edu>
2025-12-14 02:10:41 +0000

    Added genz endpoint

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -10,6 +10,9 @@ from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
 from presidio_anonymizer.entities import InvalidParamError
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 from werkzeug.exceptions import BadRequest, HTTPException
+import presidio_anonymizer.operators.genz
+from presidio_anonymizer.operators.genz import GenZ
+
 
 DEFAULT_PORT = "3000"
 
@@ -66,6 +69,33 @@ class Server:
                 operators=anonymizers_config,
             )
             return Response(anoymizer_result.to_json(), mimetype="application/json")
+        @self.app.route("/genz", methods=["POST"])
+        def genz():
+            content = request.get_json()
+            if not content:
+                raise BadRequest("Invalid request json")
+
+            # Reuse Presidio's official converter
+            analyzer_results = AppEntitiesConvertor.analyzer_results_from_json(
+            content.get("analyzer_results")
+            )
+
+            # Force genz operator
+            anonymizers_config = AppEntitiesConvertor.operators_config_from_json(
+            {
+            "DEFAULT": {
+                "type": "genz"
+            }
+            }
+            )
+
+            result = self.anonymizer.anonymize(
+            text=content.get("text", ""),
+            analyzer_results=analyzer_results,
+            operators=anonymizers_config,
+            )
+
+            return Response(result.to_json(), mimetype="application/json")
 
         @self.app.route("/deanonymize", methods=["POST"])
         def deanonymize() -> Response:
